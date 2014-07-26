0f51bcf0564dad3c96985733b5f8ce82c0df8b40
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-26 10:44:47 -0700

    main.js prints parsed data

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+var argv = require('minimist')(process.argv.slice(2));
 
+function showUsage() {
+  console.log('Usage: main.js <sleep-export.csv>');
+}
+
+if (argv._.length != 1) {
+  showUsage();
+} else {
+  var chunker = require('./chunker');
+  var parser = require('./parser');
+
+  chunker(argv._[0], function() {
+    var result = parser.apply(null, arguments);
+    console.log(result);
+  });
+}
